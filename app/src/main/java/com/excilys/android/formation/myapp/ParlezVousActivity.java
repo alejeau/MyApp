0b727dea7b7889c33f5d8c525837d0a7032def0f
Aurélien LEJEAU <alejeau@excilys.com>
2016-04-04 16:04:29 +0200

    Fourth exercise completed.

--- app/src/main/java/com/excilys/android/formation/myapp/ParlezVousActivity.java
+++ app/src/main/java/com/excilys/android/formation/myapp/ParlezVousActivity.java
@@ -2,45 +2,55 @@ package com.excilys.android.formation.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ParlezVousActivity extends AppCompatActivity {
     private final String TAG = ParlezVousActivity.class.getSimpleName();
+    private EditText usernameField;
+    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.e(TAG, "onCreate");
+//        Log.i(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.parlez_vous_main);
+        usernameField = (EditText) findViewById(R.id.editTextName);
+        passwordField = (EditText) findViewById(R.id.editTextPassword);
     }
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.e(TAG, "onDestroy");
+    public void Send(View v) {
+        String s = "";
+        if (isValid()) {
+            s += "Username: \"";
+            s += this.usernameField.getText().toString();
+            s += "\", password: \"";
+            s += this.passwordField.getText().toString();
+            s += "\"";
+        } else {
+            s = "Please fill all the fields!";
+        }
+
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.e(TAG, "onPause");
+    public void Clear(View v) {
+        this.usernameField.setText(R.string.textFieldUserName);
+        this.passwordField.setText(R.string.textFieldPassword);
     }
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.e(TAG, "onResume");
-    }
+    private boolean isValid() {
+        String s = this.usernameField.getText().toString();
+        if (s.equals("")) {
+            return false;
+        }
 
-    @Override
-    protected void onSaveInstanceState(Bundle savedInstanceState){
-        super.onSaveInstanceState(savedInstanceState);
-        Log.e(TAG, "onSaveInstanceState");
-    }
+        s = this.passwordField.getText().toString();
+        if (s.equals("")) {
+            return false;
+        }
 
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState){
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.e(TAG, "onRestoreInstanceState");
+        return true;
     }
 }
